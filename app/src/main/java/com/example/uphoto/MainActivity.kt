1bd7c16f3b0ba3e3e69b964efbe4168db876b8b3
alexyou <alexyu2023@gmail.com>
2021-10-15 23:07:04 -0700

    Initial commit

---
+++ app/src/main/java/com/example/uphoto/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.uphoto
+
+import android.content.ClipData.newIntent
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var selectButton: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        selectButton = findViewById(R.id.select_button)
+
+        selectButton.setOnClickListener{
+            val intent = Intent(this@MainActivity, SelectPhoto::class.java)
+            startActivity(intent)
+        }
+
+
+    }
+
+}
---
+++ app/src/main/java/com/example/uphoto/SelectPhoto.kt
@@ -0,0 +1,11 @@
+package com.example.uphoto
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SelectPhoto : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_select_photo)
+    }
+}
