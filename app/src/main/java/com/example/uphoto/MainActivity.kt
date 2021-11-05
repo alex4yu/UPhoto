c9be0654d46b5290d391f936716b6e4684420f4c
alexyou <alexyu2023@gmail.com>
2021-11-05 10:33:19 -0700

    about to try changes, can show photo now

--- app/src/main/java/com/example/uphoto/MainActivity.kt
+++ app/src/main/java/com/example/uphoto/MainActivity.kt
@@ -1,10 +1,8 @@
 package com.example.uphoto
 
-import android.content.ClipData.newIntent
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
@@ -13,6 +11,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         selectButton = findViewById(R.id.select_button)
 
         selectButton.setOnClickListener{
